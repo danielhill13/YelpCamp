cd155438012863327e946ec83cfb82a9e52f2ad1
Daniel Hill <daniel.hill13@gmail.com>
2017-05-05 16:51:23 -0700

    basic sign up and login

--- app.js
+++ app.js
@@ -137,7 +137,17 @@ app.post("/register", function(req, res){
         })
     })
 })
-
+//show login form
+app.get("/login", function (req, res){
+    res.render("login");
+})
+//login logic
+app.post("/login", passport.authenticate("local",
+    {
+        successRedirect: "/campgrounds",
+        failureRedirect: "/login"
+    }), function (req, res){
+})
 app.listen(3001, function(){
     console.log("YelpCamp Server Has Started");
 });
