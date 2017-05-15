ba4f9b095aad68dbcc326c1f9201fe1bf0ae7233
Daniel Hill <daniel.hill13@gmail.com>
2017-05-15 12:53:29 -0700

    added transition images to homepage, dynamic pricing

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -24,13 +24,14 @@ router.get("/new", middleware.isLoggedIn, function(req, res){
 //CREATE campground
 router.post("/", middleware.isLoggedIn, function(req, res){
     var name = req.body.name;
+    var price = req.body.price;
     var image = req.body.image;
     var description = req.body.description;
     var author = {
         id: req.user._id,
         username: req.user.username
     }
-    var newCampground = {name: name, image: image, description: description, author: author}    
+    var newCampground = {name: name, price: price, image: image, description: description, author: author}    
     //CREATE NEW CAMPGROUND AND SAVE TO DB
     Campground.create(newCampground, function(err, newCampground){
         if(err){
